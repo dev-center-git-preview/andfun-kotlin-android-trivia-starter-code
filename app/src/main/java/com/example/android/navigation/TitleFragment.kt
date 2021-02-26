5eb20e01f30bb9dd49bdfb6672a2838820085d3e
dev-center-git-preview <79448806+dev-center-git-preview@users.noreply.github.com>
2021-02-26 12:35:30 +0200

    Set "on click" listener to the "Play" button on the "Title" fragment

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
@@ -19,6 +20,13 @@ class TitleFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         super.onCreateView(inflater, container, savedInstanceState)
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
+        setListeners(binding)
         return binding.root
+    }
+
+    private fun setListeners(binding: FragmentTitleBinding) {
+        binding.playButton.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_titleFragment_to_gameFragment)
+        }
     }
 }
